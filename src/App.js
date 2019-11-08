46c7708bd75f06d307ef2ec6c1eb55b1337e0f44
Ben <bigriffin94@outlook.com>
2019-11-07 20:46:51 -0800

    Send to Repo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import React from 'react';
+import { Router } from '@reach/router';
+
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import ResponsiveNavigation from './components/ResponsiveNavigation';
+
 import './App.css';
+import logo from './logo.svg';
 
-import Menu from './menu/Menu';
+function App() {
 
-class App extends React.Component {
+  const navLinks = [
+		{
+			text: 'Home',
+			path: '/',
+			icon: 'ion-ios-home'
+		},
+		{
+			text: 'Contact',
+			path: '/contact',
+			icon: 'ion-ios-megaphone'
+		},
+		{
+			text: 'About',
+			path: '/about',
+			icon: 'ion-ios-business'
+		},
+		{
+			text: 'Blog',
+			path: '/blog',
+			icon: 'ion-ios-bonfire'
+		},
+		{
+			text: 'Portfolio',
+			path: '/portfolio',
+			icon: 'ion-ios-briefcase'
+		}
+	]
 
-  render() {
   return (
+
     <div className="App">
-      <Menu
+      <ResponsiveNavigation 
+        navLinks={ navLinks }
+		logo={ logo }
       />
+      <Router>
+        <About path="/about" />
+        <Blog path="/blog" />
+        <Contact path="/contact" />
+        <Home path="/home" />
+        <Portfolio path="/portfolio" />
+      </Router>
     </div>
-  )
- };
+
+  );
 }
 
 export default App;
